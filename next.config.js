12960d7cf5fec9e929f2b1119827b23cf3648d32
Ita Bassey <bassey.333@gmail.com>
2026-01-10 02:47:57 +0000

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  images: {
-    domains: [],
-  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
